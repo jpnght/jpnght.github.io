269998175c7809021c285521c1147192a51d64ae
jpnght <155517821+jpnght@users.noreply.github.com>
2024-01-03 14:58:42 +0800

    initial

---
+++ hello-world/src/App.js
@@ -0,0 +1,17 @@
+import logo from './singpass_logo_fullcolours.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <p>
+          <a className="App-link" href="https://q4svnfvnkb.execute-api.ap-southeast-1.amazonaws.com/dev/get-user/1" >Login to Singpass (STG)</a>          
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
