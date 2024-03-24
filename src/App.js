55cdc5372d3ec112a4820c52fa2543e458b03c0a
jpnght <155517821+jpnght@users.noreply.github.com>
2024-03-24 14:56:35 +0800

    update change

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           <a className="App-link" href="https://q4svnfvnkb.execute-api.ap-southeast-1.amazonaws.com/dev/get-user/1" >Login to SP (STG)</a>          
         </p>
+        <i>This is a demo app for testing integration with Sign APIs</i>
       </header>
     </div>
   );
